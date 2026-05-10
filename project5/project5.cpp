2bafe4594d8dfec18a88153e40a405322c0f59ef
longtphung-code <longtphung@gmail.com>
2026-05-09 21:37:03 -0400

    Final working program

--- project5/project5.cpp
+++ project5/project5.cpp
@@ -1,62 +1,30 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-
-
 using namespace std;
 
 int main()
 {
-	ifstream inputFile;
+	int rows = 10;
 
-	string fileName;
-	string townName;
-
-	int population;
-	int year = 1900;
-
-	// Ask user for town name
-	cout << "Enter the town name: ";
-	getline(cin, townName);
-
-	// Ask user for file name
-	cout << "Enter the file name: ";
-	cin >> fileName;
-
-	//open the file
-	inputFile.open(fileName);
-
-	//Validate file opening 
-	if (!inputFile)
+	//Pattern A (1-30)
+	for (int i = 1; i <= rows; i++)
 	{
-		cout << "Error: File could not be opened." << endl;
-		return 1; // Exit with error code
-	}
-
-	//Display Chart heading
-	cout << endl;
-	cout << "\n" << townName << " Population Growth " << endl;
-	cout << "(each * represents 1,000 people)" << endl;
-
-	//Read population data from file and display chart
-	while (inputFile >> population)
-	{
-		//Display Year
-		cout << year << " ";
-
-		//Display bar chart
-		for (int count = 0; count < population / 1000; count++)
+		for (int j = 1; j <= i; j++)
 		{
-			cout << "*";
-
+			cout << "+";
 		}
 		cout << endl;
-
-		// Increase year by 20
-		year += 20;
 	}
-	//Close the file
-	inputFile.close();
+
+	cout << endl;
+	//Pattern B (10-1)
+	for (int i = rows; i >= 1; i--)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout << "+";
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
